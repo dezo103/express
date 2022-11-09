db11c1ac14dc78e2aa9b5b1ad17010c049e541cb
dezo103 <dezo103@gmail.com>
2022-11-09 22:21:04 +0300

    feat uuuu

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Dezo!')
+    res.send('Hello Dezouuuuuuu!')
 })
 
 app.listen(port, () => {
