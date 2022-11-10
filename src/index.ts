8d7d323996cf8a36530f95245ac302f94e504ce3
dezo103 <dezo103@gmail.com>
2022-11-10 11:18:39 +0300

    fixed name

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello DezoOOO!')
+    res.send('Hello Dezo!')
 })
 
 app.listen(port, () => {
